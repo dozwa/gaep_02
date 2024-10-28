69138df1f3e8d93ebbc2afe8ad3b975aa0a65e50
NoelLD <Leon.Dodrimong@web.de>
2024-10-28 18:02:42 +0100

    added routing

--- Frontend/app/GAEP/src/app/app.routes.ts
+++ Frontend/app/GAEP/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SearchHistoryComponent } from './components/search-history/search-history.component';
+import { TutorialComponent } from './components/tutorial/tutorial.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'tutorial', component: TutorialComponent },
+    { path: 'searchHistory', component: SearchHistoryComponent }
+    
+];
