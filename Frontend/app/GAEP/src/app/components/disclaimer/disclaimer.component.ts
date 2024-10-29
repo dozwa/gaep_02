ec4cfb818eada4476b726bca6db9e801f1ec7088
NoelLD <Leon.Dodrimong@web.de>
2024-10-29 12:25:14 +0100

    added custom color theme (template) and adapt menu bar and disclaimer

--- Frontend/app/GAEP/src/app/components/disclaimer/disclaimer.component.ts
+++ Frontend/app/GAEP/src/app/components/disclaimer/disclaimer.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatButton} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-disclaimer',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule,MatButton,FormsModule],
   templateUrl: './disclaimer.component.html',
   styleUrl: './disclaimer.component.scss',
 })
 export class DisclaimerComponent {
   constructor(private router: Router){}
+
+  acceptedCookies:Boolean = false;
+  acceptedInformation:Boolean = false;
+  acceptedUsage:Boolean = false;
+  
 
   onDisclaimerSend() {
 
