9d9b7f2f57706c1920bec5eb7aaba159d305f5c3
NoelLD <Leon.Dodrimong@web.de>
2024-10-29 12:56:24 +0100

    added responsive design to menu bar

--- Frontend/app/GAEP/src/app/components/header/header.component.ts
+++ Frontend/app/GAEP/src/app/components/header/header.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FormsModule } from '@angular/forms';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule,MatSidenavModule,FormsModule,FlexLayoutModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
