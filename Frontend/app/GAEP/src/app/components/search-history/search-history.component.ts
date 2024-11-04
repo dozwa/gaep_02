480377c32a4cc0be763caf07406e38358d9bad62
Ruben <s0563402@htw-berlin.de>
2024-11-04 13:55:04 +0100

    Add  header and *ngFor to search-history

--- Frontend/app/GAEP/src/app/components/search-history/search-history.component.ts
+++ Frontend/app/GAEP/src/app/components/search-history/search-history.component.ts
@@ -2,11 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-history',
-  standalone: true,
-  imports: [],
   templateUrl: './search-history.component.html',
-  styleUrl: './search-history.component.scss'
+  styleUrls: ['./search-history.component.css']
 })
 export class SearchHistoryComponent {
+  exampleQuestions = [
+    { question: 'Sind NSARs bei chronischem Kreuzschmerz empfehlenswert?', type: 'einfach' },
+    { question: 'Welche Übungen werden für Kreuzschmerzen empfohlen?', type: 'detailliert' },
+    { question: 'Wie können psychosoziale Risikofaktoren bei Kreuzschmerzen erkannt werden?', type: 'detailliert' },
+    { question: 'Ist eine Bildgebung zur Diagnose von Kreuzschmerzen sinnvoll?', type: 'einfach' },
+    { question: 'Welches Training wird bei COPD empfohlen?', type: 'detailliert' },
+    { question: 'Wird Atemtherapie in der Behandlung von COPD empfohlen?', type: 'einfach' },
+    { question: 'Wie kann ich bei COPD eine Tabakentwöhnung durchführen?', type: 'detailliert' }
+  ];
 
+  onQuestionClick(question: { question: string, type: string }) {
+    // Logik bei Klick auf eine Frage
+    console.log(`Ausgewählte Frage: ${question.question}, Antworttyp: ${question.type}`);
+    // Hier können Sie zusätzliche Logik implementieren
+  }
 }
