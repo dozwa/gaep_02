8ca1ff0be3541a76dff3fb9a40f87a71f53c0a73
NoelLD <Leon.Dodrimong@web.de>
2024-11-05 12:22:25 +0100

    continued feedback modal

--- Frontend/app/GAEP/src/app/modals/feedback-modal/feedback-modal.component.ts
+++ Frontend/app/GAEP/src/app/modals/feedback-modal/feedback-modal.component.ts
@@ -3,18 +3,30 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {MatButton} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-feedback-modal',
   templateUrl: './feedback-modal.component.html',
   standalone: true,
-  imports : [MatFormFieldModule,MatInputModule,MatButton],
+  imports : [MatFormFieldModule,MatInputModule,MatButton, FormsModule],
   styleUrl: './feedback-modal.component.scss'
 })
 export class FeedbackModalComponent {
+
+  public subject:string = "";
+  public comment:string="";
+  public mail:string="";
+
   constructor(public dialogRef: MatDialogRef<FeedbackModalComponent>) {}
+
   onClose(): void {
     this.dialogRef.close();
+  }
+  onClear(){
+    this.subject="";
+    this.comment="";
+    this.mail="";
   }
 }
    
